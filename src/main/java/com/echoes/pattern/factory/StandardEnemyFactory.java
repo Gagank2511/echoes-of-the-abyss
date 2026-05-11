2b577ca67a45aa3d3aaedd9e109af1edffc6ddf9
Kaur <gagandeepmalh@gmail.com>
2026-05-12 00:09:03 +0100

    feat: implement StandardEnemyFactory for normal difficulty enemies

---
+++ src/main/java/com/echoes/pattern/factory/StandardEnemyFactory.java
@@ -0,0 +1,27 @@
+package com.echoes.pattern.factory;
+
+import com.echoes.model.character.enemy.DragonWhelp;
+import com.echoes.model.character.enemy.Enemy;
+import com.echoes.model.character.enemy.EnemyType;
+import com.echoes.model.character.enemy.Goblin;
+import com.echoes.model.character.enemy.Skeleton;
+import com.echoes.model.character.enemy.Troll;
+import com.echoes.model.character.enemy.boss.AbyssDragon;
+import com.echoes.model.character.enemy.boss.Lich;
+import com.echoes.model.character.enemy.boss.StoneGolem;
+
+public final class StandardEnemyFactory implements EnemyFactory {
+
+    @Override
+    public Enemy create(final EnemyType type, final int floorLevel) {
+        return switch (type) {
+            case GOBLIN       -> new Goblin(floorLevel);
+            case SKELETON     -> new Skeleton(floorLevel);
+            case TROLL        -> new Troll(floorLevel);
+            case DRAGON_WHELP -> new DragonWhelp(floorLevel);
+            case STONE_GOLEM  -> new StoneGolem(floorLevel);
+            case LICH         -> new Lich(floorLevel);
+            case ABYSS_DRAGON -> new AbyssDragon(floorLevel);
+        };
+    }
+}
