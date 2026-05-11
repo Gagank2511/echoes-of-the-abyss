ed2cefb081de4b8917edf5b77075219b070842c7
Kaur <gagandeepmalh@gmail.com>
2026-05-12 00:09:04 +0100

    feat: implement EliteEnemyFactory with boosted stats for harder variants

---
+++ src/main/java/com/echoes/pattern/factory/EliteEnemyFactory.java
@@ -0,0 +1,30 @@
+package com.echoes.pattern.factory;
+
+import com.echoes.model.character.enemy.DragonWhelp;
+import com.echoes.model.character.enemy.Enemy;
+import com.echoes.model.character.enemy.EnemyType;
+import com.echoes.model.character.enemy.Goblin;
+import com.echoes.model.character.enemy.Skeleton;
+import com.echoes.model.character.enemy.Troll;
+import com.echoes.model.character.enemy.boss.AbyssDragon;
+import com.echoes.model.character.enemy.boss.Lich;
+import com.echoes.model.character.enemy.boss.StoneGolem;
+
+public final class EliteEnemyFactory implements EnemyFactory {
+
+    private static final int ELITE_LEVEL_BOOST = 1;
+
+    @Override
+    public Enemy create(final EnemyType type, final int floorLevel) {
+        final int boosted = floorLevel + ELITE_LEVEL_BOOST;
+        return switch (type) {
+            case GOBLIN       -> new Goblin(boosted);
+            case SKELETON     -> new Skeleton(boosted);
+            case TROLL        -> new Troll(boosted);
+            case DRAGON_WHELP -> new DragonWhelp(boosted);
+            case STONE_GOLEM  -> new StoneGolem(boosted);
+            case LICH         -> new Lich(boosted);
+            case ABYSS_DRAGON -> new AbyssDragon(boosted);
+        };
+    }
+}
