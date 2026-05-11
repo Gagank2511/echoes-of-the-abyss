6e50e4a9db3bcd1eb18ad8c19ffa7a34a7cbe441
Kaur <gagandeepmalh@gmail.com>
2026-05-12 00:09:37 +0100

    feat: add GameEventType enum for all observable game events

---
+++ src/main/java/com/echoes/pattern/observer/GameEventType.java
@@ -0,0 +1,13 @@
+package com.echoes.pattern.observer;
+
+public enum GameEventType {
+    PLAYER_ATTACKED,
+    ENEMY_ATTACKED,
+    PLAYER_FLED,
+    PLAYER_DEFEATED,
+    ENEMY_DEFEATED,
+    ITEM_USED,
+    PLAYER_LEVELLED_UP,
+    BOSS_DEFEATED,
+    DUNGEON_CLEARED
+}
